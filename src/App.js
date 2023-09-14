ab675994f1dd2d88978d12dbf08e207e6ebec1ba
Fritz van Deventer <fritzvd@users.noreply.github.com>
2023-09-14 11:15:13 +0200

    functionify

--- src/App.js
+++ src/App.js
@@ -1,56 +1,26 @@
-import React from 'react';
-import Logo from './Logo';
-import './App.css';
-import Preferences from './Preferences';
+import React, { useState } from "react";
+import Logo from "./Logo";
+import "./App.css";
+import Preferences from "./Preferences";
 
-class App extends React.Component{
-  constructor(props) {
-    super(props)
-    this.state = {
-      preferences: {
-        logoColor: 'salmon',
-        companyName: ''
-      }
-    }
-  }
+function App() {
+  const [preferences, setPreferences] = useState({
+    logoColor: "ochre",
+    companyName: "myCompany Name is Marvelous",
+  });
 
-  setColor (color) {
-    this.setState((state) => (
-      {preferences: {...state.preferences, color: color}}
-      )
-    ) /// Deze functie returned de verandering met de nieuwe preferences, omdat we
-      /// meerdere eigenschappen in Preferences beheren.
-      /// Maar als we niet de rest van de preferences doorpassen, zijn we de kleur kwijt
-
-      /// Probeer onderstaande maar eens:
-      ///this.setState({preferences: {color: color}})  }
-  }
-  
-  setCompanyName (companyName) {
-    this.setState((state) => (
-      {preferences: {...state.preferences, companyName: companyName}}
-      )
-    ) /// Deze functie returned de verandering met de nieuwe preferences, omdat we
-      /// meerdere eigenschappen in Preferences beheren.
-      /// Maar als we niet de rest van de preferences doorpassen, zijn we de kleur kwijt
-
-      /// Probeer onderstaande maar eens:
-      ///this.setState({preferences: {companyName: companyName}})
-  }
-
-  render () {
-
-    const boundSetColor = (c) => this.setColor(c)
-    const setCompanyName = this.setCompanyName.bind(this)
-    return <div className="App">
-      <Logo logoColor={this.state.preferences.logoColor} companyName={this.state.preferences.companyName} />
+  return (
+    <div className="App">
+      <Logo
+        logoColor={preferences.logoColor}
+        companyName={preferences.companyName}
+      />
       <Preferences
-        preferences={this.state.preferences}
-        setColor={boundSetColor}
-        setCompanyName={setCompanyName}
+        preferences={preferences}
+        setUpstreamPrefs={(prefs) => setPreferences(prefs)}
       />
     </div>
-  }
+  );
 }
 
 export default App;
--- src/Preferences.jsx
+++ src/Preferences.jsx
@@ -1,47 +1,53 @@
-import React from 'react'
-import App from './App';
-import { thisTypeAnnotation } from '@babel/types';
+import React, { useState } from "react";
 
-export default class Preferences extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      logoColor: props.preferences.logoColor,
-      companyName: props.preferences.companyName
-    }
+export default function Preferences(props) {
+  const [localPrefs, setLocalPrefs] = useState({
+    sidekicks: ["Robin", "Sparten", "Ant-Man"],
+    ceos: ["Jeff MF Bezos", ""],
+    logoColor: props.preferences.logoColor,
+    companyName: props.preferences.companyName,
+  });
+
+  const logoColorChangeHandler = (e) => {
+    console.log(e.target.value);
+    setLocalPrefs({
+      ...localPrefs,
+      logoColor: e.target.value,
+    });
+  };
+
+  const companyNameHandler = (e) =>
+    setLocalPrefs({ ...localPrefs, companyName: e.target.value });
+
+  const clearCompanyName = () =>
+    setLocalPrefs({ ...localPrefs, companyName: "" });
+
+  function onSubmit(e) {
+    e.preventDefault();
+    props.setUpstreamPrefs(localPrefs);
+    clearCompanyName();
   }
 
-  logoColorChangeHandler (event) {
-    this.setState({logoColor: event.target.value})
-  }
-
-  companyChangeHandler (event) {
-    this.setState({companyName: event.target.value})
-  }
-
-  onSubmit (event) {
-    event.preventDefault();
-    this.props.setColor(this.state.logoColor)
-    this.props.setCompanyName(this.state.companyName)
-    this.clearCompanyName();
-  }
-
-  clearCompanyName () {
-    this.setState({companyName: ''})
-  }
-
-  render() {
-    return <form onSubmit={(e) => this.onSubmit(e)}>
-      <select onChange={(e) => this.logoColorChangeHandler(e)} value={this.state.logoColor}>
+  return (
+    <form onSubmit={(e) => onSubmit(e)}>
+      <select
+        onChange={(e) => logoColorChangeHandler(e)}
+        value={localPrefs.logoColor}
+      >
         <option>red</option>
         <option>salmon</option>
         <option>ochre</option>
         <option>seagreen</option>
       </select>
-      <input type="text" placeholder="Type the company name" onChange={(e) => this.companyChangeHandler(e)} value={this.state.companyName}/>
-      <input type="submit" value='Save Preferences' />
-      <div className={`preview ${this.state.logoColor}`}>Preview for: {this.state.companyName}</div>
+      <input
+        type="text"
+        placeholder="Type the company name"
+        onChange={companyNameHandler}
+      />
+      <input type="submit" value="Save Preferences" />
+      <div className={`preview ${localPrefs.logoColor}`}>
+        Preview for: {localPrefs.companyName}
+      </div>
     </form>
-  }
-
+  );
 }
